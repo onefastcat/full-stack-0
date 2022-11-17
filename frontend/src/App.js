8aa784f4c9b88178423dea8509da37f46861727a
onefastcat <a.kho.meow@gmail.com>
2022-11-17 10:50:56 -0500

    login and signup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,27 @@
+// frontend/src/App.js
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+import SignupFormPage from "./components/SignUpFormPage";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
+      </Route>
+    </Switch>
   );
 }
 
